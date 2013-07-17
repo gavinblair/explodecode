d4d9a49443875584705cb79dd9abff8f6690163e
gavinblair <gavin.blair@rtraction.com>
2013-07-17 07:29:30 -0400

    just about done the front page. there is a bug with the tabs when you first pick a tab

--- front.php
+++ front.php
@@ -36,66 +36,49 @@ Template Name: Home Template
                   with office hours. &nbsp; &nbsp; &nbsp; &nbsp;
                   <br> &nbsp;
                   <br>
-                  <a class="btn btn-primary btn-large">Only $99 - Register Now</a> 
+                  <a class="btn btn-primary btn-large">Only $99 - Register Now</a>
+                   <br /> <br /><a href="workshops">Learn More</a>
                 </p>
               </div>
               <div id="talks" class='tab'>
                 <h1 class="center">A Community of Developers</h1>
-                <p class="center" id="intro">In September 2013, we're running our first workshop on Phonegap. Learn
-                  how to use your existing web development skills to build and deploy mobile
-                  apps for multiple devices and stores. Get one-on-one time with local experts
-                  with office hours. &nbsp; &nbsp; &nbsp; &nbsp;
+                <p class="center" id="intro">Talks features two or three local developers as they show off some of their code and tell the story behind it.
+                Hosted at Big Viking Games, Talks is an opportunity to meet and learn from developers in the community. &nbsp; &nbsp; &nbsp; &nbsp;
                   <br> &nbsp;
                   <br>
-                  <a class="btn btn-primary btn-large">Only $99 - Register Now</a> 
+                  <a class="btn btn-primary btn-large">Free event: September 12 - Register Now</a> 
+                  <br /> <br /><a href="talks">Learn More</a>
                 </p>
               </div>
               <div id="helloworld" class='tab'>
                 <h1 class="center">Learn Something New</h1>
-                <p class="center" id="intro">In September 2013, we're running our first workshop on Phonegap. Learn
-                  how to use your existing web development skills to build and deploy mobile
-                  apps for multiple devices and stores. Get one-on-one time with local experts
-                  with office hours. &nbsp; &nbsp; &nbsp; &nbsp;
+                <p class="center" id="intro">Hello, World! is an opportunity to learn something new. 
+                  Pick a programming language or framework that you don't know and build something with it - anything!
+                  Then, bring your work to this event, show it off like it's the next Facebook, and eat pizza!
+                  This is an open event and everyone is invited to participate. &nbsp; &nbsp; &nbsp; &nbsp;
                   <br> &nbsp;
                   <br>
-                  <a class="btn btn-primary btn-large">Only $99 - Register Now</a> 
+                  <a class="btn btn-primary disabled btn-large">Next event: November 2013 - Registration opening soon</a> 
+                  <br /> <br /><a href="hello-world">Learn More</a>
                 </p>
               </div>
             </div>
       <p class="center"><?php the_title(); ?></p>
-      <div class="hidden-phone row">
-        <a href="#" class="thumb align-center offset25 span3">
-      <img src="http://i.imgur.com/9xIliL2.png">
-      <br>
-      <strong class="caption">MongoDB</strong> 
-    </a>
-        <a href="#" class="thumb align-center span3">
-      <img src="http://i.imgur.com/byDYh6P.png">
-      <br>
-      <strong class="caption">Responsive</strong> 
-    </a>
-        <a href="#" class="thumb align-center span3">
-      <img src="http://i.imgur.com/byDYh6P.png">
-      <br>
-      <strong class="caption">Responsive</strong> 
-    </a> 
-      </div>
-      <br>
-      <div class="hidden-phone row">
-        <a href="#" class="thumb align-center offset25 span3">
-      <img src="http://i.imgur.com/9xIliL2.png">
-      <br>
-      <strong class="caption">MongoDB</strong> 
-    </a>
-        <a href="#" class="thumb align-center span3">
-      <img src="http://i.imgur.com/byDYh6P.png">
-      <br>
-      <strong class="caption">Responsive</strong> 
-    </a>
-        <a href="#" class="thumb align-center span3">
-      <img src="http://i.imgur.com/byDYh6P.png">
-      <br>
-      <strong class="caption">Responsive</strong> 
-    </a> 
-      </div>
+      <?php query_posts(array('post_type' => 'post')); ?>
+      <?php $c=-1; if ( have_posts() ) : while ( have_posts() ) : the_post(); $c++; ?>
+        <?php if($c%3 || $c==0){?>
+          <div class="hidden-phone row">
+        <?php } ?>
+          <a href="#" class="thumb align-center <?php if($c==1 || ($c-2)%3){ ?>offset25 <?php } ?> span3">
+             <img src="http://i.imgur.com/9xIliL2.png">
+             <br>
+             <strong class="caption"><?php the_title(); ?></strong> 
+          </a>
+        <?php if(($c-1)%3 && $c>3){?>
+          </div><br />
+        <?php } ?>
+    <?php endwhile; endif; ?>
+    <?php if(($c-1)%3 || $c<=3){ ?>
+      </div><br />
+    <?php } ?>
 <?php get_footer(); ?>
