f4445b5abbb73e01c9d1935803b54b41f4e92253
gavinblair <gavin.blair@rtraction.com>
2013-07-23 07:19:14 -0400

    page template

---
+++ page.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+            <img src="http://i.imgur.com/IyyXt6d.png">
+            <div class="container">
+                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                    <div class="row article">
+                        <div class="span8 offset2">
+                            <h2><?php the_title(); ?></h2>
+                            <?php the_content(); ?>
+                            <?php comments_template( '', true ); ?>
+                        </div>
+                    </div>
+                <?php endwhile; else: ?>
+                <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+                <?php endif; ?>
+            </div>
+<?php get_footer(); ?>
+            
